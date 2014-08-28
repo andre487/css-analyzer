2eadeb197b057ef1f1e9eedbe9cf1fccdbe4b2c0
Andre-487 <andrey.prokopyuk@gmail.com>
2014-08-28 11:26:57 +0400

    Add arguments parser

--- modules/bem-csser.js
+++ modules/bem-csser.js
@@ -1,4 +1,27 @@
 'use strict';
+var packageInfo = require('./package-info.js');
+
 module.exports = function () {
-    console.log('test');
+    var argParser = buildArgParser();
+    console.log(argParser.parseArgs());
 };
+
+function buildArgParser() {
+    var ArgumentParser = require('argparse').ArgumentParser,
+        parser = new ArgumentParser({
+            version: packageInfo.version,
+            description: packageInfo.description,
+            addHelp: true,
+            prog: 'bem-csser'
+        });
+    parser.addArgument(
+        ['action'],
+        {
+            action: 'store',
+            choices: ['help'],
+            required: true,
+            help: 'What to do'
+        }
+    );
+    return parser;
+}
